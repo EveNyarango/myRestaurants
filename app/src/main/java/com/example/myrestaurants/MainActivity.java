49cb7a644b2ef4dacc9bdc444954669951ea274b
EveNyarango <evenyarango@gmail.com>
2020-12-29 08:39:09 +0300

    create a toast

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
@@ -18,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
+
         });
     }
 }
